324aa0bb0352be8e6195b2e9bb4aee35fc9503d2
Israel Fertig <m583288718@gmail.com>
2024-07-21 13:26:13 +0300

    Clean code without CLI

--- functions.py
+++ functions.py
@@ -1,31 +1,26 @@
-from classes import *
+from classes import A_Question, Player
 import random
 
 
-def createTriviaQuestion(question):
+def create_trivia_question(question):
     question_object = A_Question(question["question"],
                                  question["answers"],
                                  question["correct_answer"])
     return question_object
 
 
-def createQuestionList(lst):
-    questions = []
-    for question in lst:
-        questions.append(createTriviaQuestion(question))
-    return questions
+def create_question_list(lst_dict_questions):
+    list_objects_questions = []
+    for question in lst_dict_questions:
+        list_objects_questions.append(create_trivia_question(question))
+    return list_objects_questions
 
 
-def createPlayer(name):
-    a = Player(name)
-    return a
-
-
-def createPlayerList(listPlayersName):
-    playersList = []
-    for player in listPlayersName:
-        playersList.append(createPlayer(player))
-    return playersList
+def create_player_list(list_players_name):
+    players_list = []
+    for player_name in list_players_name:
+        players_list.append(Player(player_name))
+    return players_list
 
 
 def print_question(player, question):
@@ -36,12 +31,11 @@ def print_question(player, question):
 def print_random_answers(question):
     answers_list = list(question.answers.values())
     random.shuffle(answers_list)
-    # מדפיסים את התשובות בסדר אקראי, אבל עם מספרים מסודרים
+    # Create a new mapping
+    new_mapping = {str(i): key for i, (key) in enumerate(answers_list, 1)}
+    # The answers are printed in random order, but with ordered numbers
     for i, (value) in enumerate(answers_list, 1):
         print(f"{i}. {value}")
-    #
-    # # יוצרים מיפוי חדש
-    new_mapping = {str(i): key for i, (key) in enumerate(answers_list, 1)}
     return new_mapping
 
 
@@ -49,10 +43,25 @@ def get_player_answer():
     return input("Please enter your answer")
 
 
-def handle_correct_answer(player, questions, random_item):
+def answer_is_correct(player):
     print(f"Very Good, correct answer Mr, {player.name}")
     player.score += 1
 
 
+def answer_is_not_correct(player):
+    print(f"I'm sorry Mr {player.name}, wrong answer")
+
+
 def find_winner(players):
     return max(players, key=lambda player: player.score)
+
+
+def display_instructions():
+    print("""
+Game Instructions:
+1. The game presents trivia questions to each player in turn.
+2. Answer the question by choosing the number corresponding to the answer.
+3. A correct answer earns a point and the player continues to the next question.
+4. The game continues until all questions have been asked.
+5. The player with the highest score wins.
+""")
--- main.py
+++ main.py
@@ -1,31 +1,40 @@
-from functions import *
-from json_functions import *
+from functions import (create_question_list, create_player_list,
+                       print_question, print_random_answers, get_player_answer,
+                       answer_is_correct, find_winner, display_instructions,
+                       answer_is_not_correct)
+from json_functions import load_questions
+import random
+import argparse
 
 
 def main():
-    lst = load_questions("trivia_questions.json")
+    display_instructions()
+    lst_dict_questions = load_questions("trivia_questions.json")
     num_of_players = int(input("Please enter the number of players"))
     players = []
     for player in range(num_of_players):
         players.append(input("enter name player"))
-    players = createPlayerList(players)
-    questions = (createQuestionList(lst))
-    random_item = random.choice(questions)
-    current_player = 0
-    while questions:
-        for player in players:
-            print_question(player, random_item)
-            new_mapping = print_random_answers(random_item)
+    list_players_object = create_player_list(players)
+    random.shuffle(list_players_object)
+    questions = create_question_list(lst_dict_questions)
+    random_question = random.choice(questions)
+    while len(questions) > 0:
+        for player in list_players_object:
+            print_question(player, random_question)
+            new_mapping = print_random_answers(random_question)
             player_answer = get_player_answer()
-            if new_mapping[player_answer] == random_item.correct_answer:
-                handle_correct_answer(player, questions, random_item)
+            if new_mapping[player_answer] == random_question.correct_answer:
+                answer_is_correct(player)
                 if len(questions) > 1:
-                    questions.remove(random_item)
-                    random_item = random.choice(questions)
+                    questions.remove(random_question)
+                    random_question = random.choice(questions)
                 else:
                     questions.pop(0)
                     break
-    winner = find_winner(players)
+            else:
+                answer_is_not_correct(player)
+
+    winner = find_winner(list_players_object)
     print(f"You win Mr. {winner.name} you got {winner.score} points")
 
 
